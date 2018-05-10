d501a2be248847404e77fdb5a54019855e0a424c
Tim Moore <timmoore226@gmail.com>
2018-05-10 12:10:14 -0400

    Taste the rainbow button

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
 let screen = document.querySelector("#screen");
 let clearBtn = document.querySelector("#clear-btn");
+let rainbowBtn = document.querySelector("#rainbow-btn");
 
 let color = '#3a3a3a';
+let rainbow = false;
 
 function createGrid(size) {
   const numberOfCells = Math.pow(size, 2);
 
-  gridElements = "auto ".repeat(size).trim();
+  gridElements = "auto ".repeat(size);
 
   for(let x = 0; x < numberOfCells; x++){
     	let cell = document.createElement("div");
@@ -16,22 +18,38 @@ function createGrid(size) {
   screen.setAttribute('style', `grid-template-columns: ${gridElements}; grid-template-rows: ${gridElements};`);
 }
 
+let getGridCells = () => {
+  const cells = document.querySelectorAll('.grid-cell');
+  return cells;
+}
+
+function randomColor () {
+    var max = 0xffffff;
+    return '#' + Math.round( Math.random() * max ).toString( 16 );
+}
+
 let drawColor = (e) => {
-    e.target.style.backgroundColor = color
+  color = rainbow ? randomColor() : '#3a3a3a';
+  e.target.style.backgroundColor = color;
 }
 
 let clearGrid = () => {
-  const gridCells = document.querySelectorAll('.grid-cell');
+  const gridCells = getGridCells();
 	gridCells.forEach(gridCell => gridCell.style.backgroundColor = "white");
+}
+
+let drawRainbowColors = () => {
+  rainbow = rainbow ? false : true;
 }
 
 function init() {
   const defaultSize = 16;
   createGrid(defaultSize);
 
-  const gridCells = document.querySelectorAll('.grid-cell');
+  const gridCells = getGridCells();
   gridCells.forEach(gridCell => gridCell.addEventListener("mouseenter", drawColor));
 
+  rainbowBtn.addEventListener("click", drawRainbowColors);
   clearBtn.addEventListener("click", clearGrid);
 }
 
