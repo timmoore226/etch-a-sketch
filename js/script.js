672d6ead9d221b03b816bbb9fca6bc24944969bf
Tim Moore <timmoore226@gmail.com>
2018-05-10 23:24:02 -0400

    Added function to change screen resolution

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 let screen = document.querySelector("#screen");
 let clearBtn = document.querySelector("#clear-btn");
-let rainbowBtn = document.querySelector("#rainbow-btn");
+let colorBtn = document.querySelector("#color-btn");
+let resizeBtn = document.querySelector("#resize-btn");
 
 let color = '#3a3a3a';
 let rainbow = false;
+let gridSize = 16;
 
 function createGrid(size) {
   const numberOfCells = Math.pow(size, 2);
@@ -16,6 +18,9 @@ function createGrid(size) {
     	screen.appendChild(cell);
   }
   screen.setAttribute('style', `grid-template-columns: ${gridElements}; grid-template-rows: ${gridElements};`);
+
+  const gridCells = getGridCells();
+  gridCells.forEach(gridCell => gridCell.addEventListener("mouseenter", drawColor));
 }
 
 let getGridCells = () => {
@@ -38,19 +43,31 @@ let clearGrid = () => {
 	gridCells.forEach(gridCell => gridCell.style.backgroundColor = "white");
 }
 
-let drawRainbowColors = () => {
+let switchColors = () => {
   rainbow = rainbow ? false : true;
+  if(rainbow) {
+    console.log(colorBtn.value);
+    colorBtn.innerText = "Color Black";
+  } else {
+    console.log(colorBtn.value);
+    colorBtn.innerText = "Color Rainbows";
+  }
+}
+
+let resizeGrid = () => {
+  gridSize = prompt("What size would you like?");
+  while (screen.firstChild) {
+    screen.removeChild(screen.firstChild);
+  }
+  createGrid(gridSize);
 }
 
 function init() {
-  const defaultSize = 16;
-  createGrid(defaultSize);
+  createGrid(gridSize);
 
-  const gridCells = getGridCells();
-  gridCells.forEach(gridCell => gridCell.addEventListener("mouseenter", drawColor));
-
-  rainbowBtn.addEventListener("click", drawRainbowColors);
+  colorBtn.addEventListener("click", switchColors);
   clearBtn.addEventListener("click", clearGrid);
+  resizeBtn.addEventListener("click", resizeGrid);
 }
 
 init();
